e7e7ab79b599e55196593243af5895a3c49ebb5e
DEM773 <gzaid037@gmail.com>
2026-04-02 22:08:43 -0600

    Add files via upload

---
+++ Untitled.cpp
@@ -0,0 +1,20 @@
+#include "Alumnos.h"
+#include <iostream>
+
+using namespace std;
+
+Alumno::Alumno(){
+    cout << "Constructor ejecutado\n";
+}
+
+Alumno::~Alumno(){
+    cout << "Destructor ejecutado\n";
+}
+
+void Alumno::msg(){
+    cout << "Salida vacía\n";
+}
+
+void Alumno::msg(int a){
+    cout << "Salida valor: " << a << endl;
+}
